d1532568fea035ccf367ab0df1289f54746b84fb
tongh <3272534298@qq.com>
2019-12-27 17:32:30 +0800

    新增config.yaml，完善配置文件加载方式

--- app/config/config.go
+++ app/config/config.go
@@ -8,35 +8,6 @@ import (
 	"time"
 )
 
-const (
-	Debug = true
-
-	ServerAddr = "0.0.0.0:3335" //开发机、测试机
-	ReadTimeout = 3*time.Second
-	WriteTimeout = 5*time.Second
-
-	//开发机
-	MysqlDataSource = "root:hello123@tcp(127.0.0.1:3306)/message" +
-		"?timeout=3s&collation=utf8mb4_unicode_ci&readTimeout=5s&writeTimeout=5s"
-	MaxOpenConns    = 2000
-	MaxIdleConns    = 1000
-    ConnMaxLifetime = 7200 * time.Second
-    TimeZone        = "Asia/Shanghai"
-
-	//测试机
-	//MysqlDataSource = "root:xC6ch6I4u0X3h@tcp(127.0.0.1:3306)/message" +
-	//		"?timeout=3s&collation=utf8mb4_unicode_ci&readTimeout=5s&writeTimeout=5s"
-
-
-	AmqpUrl       = "amqp://guest:guest@localhost:5672/" //开发机、测试机
-	ConnectionNum = 10
-	ChannelNum    = 100
-
-	OssDomain = "http://image.com"
-
-
-	)
-
 type Config struct {
 	Web Web
 	Mysql Mysql
